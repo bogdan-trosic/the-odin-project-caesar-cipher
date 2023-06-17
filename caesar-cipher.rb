cff6bef99b3e4faa1c87d1e74e1c7fcdacbe64c3
Bogdan Trosic <trosic.bogdan.95@gmail.com>
2023-06-18 01:51:15 +0200

    project caesar cipher init

---
+++ caesar-cipher.rb
@@ -0,0 +1,28 @@
+def caesar_cipher(text, offset)
+  split_arr = text.split("")
+  cipher_arr = split_arr.map do |char|
+    char_int = char.to_i(36)
+    max_int = 35
+    offset_base = 9
+    
+    if char == " " || char == "." || char == "," || char == "!" || char == "?"
+      char
+    elsif char_int + offset > max_int
+      if char.upcase == char
+        (offset - (max_int - char_int) + offset_base).to_s(36).upcase
+      else
+        (offset - (max_int - char_int) + offset_base).to_s(36)
+      end
+    else
+      if char.upcase == char
+        (char_int + offset).to_s(36).upcase
+      else
+        (char_int + offset).to_s(36)
+      end
+    end
+  end
+
+  p cipher_arr.join
+end
+
+caesar_cipher("What a string!", 5)
